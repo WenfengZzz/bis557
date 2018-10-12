4e9d455b1cd96562e87091a3546c8efc239b18f4
Wenfeng Zhang <wenfeng@Wenfengs-MBP.wireless.yale.internal>
2018-10-12 19:15:02 -0400

    added data description

--- R/data-doc.r
+++ R/data-doc.r
@@ -8,3 +8,18 @@
 #' on linear regressions. This data set gives us a basis for exploration.
 NULL
 
+#' A training data set for the second homework
+#'
+#' @name ridge_train
+#' @docType data
+#' @rdname ridge_train
+#' @description A training dataset for ridge regression in homework2.
+"ridge_train"
+
+#' A testing data set for the second homework
+#'
+#' @name ridge_test
+#' @docType data
+#' @rdname ridge_test
+#' @description A testing dataset for ridge regression in homework2.
+"ridge_test"
