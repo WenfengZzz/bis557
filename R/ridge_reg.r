6bb06ee6e7552101e7a943ec03859db7a9dfc6da
Wenfeng Zhang <wenfeng@Wenfengs-MBP.wireless.yale.internal>
2018-10-13 13:04:38 -0400

    updated SVD

--- R/ridge_reg.r
+++ R/ridge_reg.r
@@ -14,14 +14,22 @@ ridge_reg <- function(formula, lambda, data){
 	
 	x <- as.matrix(model.matrix(formula, data))
 	y <- data[,as.character(formula)[2]]
-	beta <- solve(t(x)%*%x+lambda*diag(dim(x)[2])) %*% t(x) %*% y
+	
+  svd_obj <- svd(x)
+  U <- svd_obj[["u"]]
+  V <- svd_obj[["v"]]
+  svals <- svd_obj[["d"]]
+  D <- diag(svals / (svals^2 + lambda))
+  beta <- V %*% D %*% t(U) %*% y
+
+  # beta <- solve(t(x)%*%x+lambda*diag(dim(x)[2])) %*% t(x) %*% y
 
 	# Format coef for export
-  	coef <- as.numeric(beta)
-  	names(coef) <- colnames(x)
+  coef <- as.numeric(beta)
+  names(coef) <- colnames(x)
 
-  	# The object to export
-  	ret <- list(coef = coef, lambda = lambda, formula = formula)
-  	class(ret) <- "ridge_reg"
-  	ret
+  # The object to export
+  ret <- list(coef = coef, lambda = lambda, formula = formula)
+  class(ret) <- "ridge_reg"
+  ret
 }
